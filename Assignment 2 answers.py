0541d7f00faf41585b6adfb4c907525a85d5983a
Sukhman21103060 <97335744+Sukhman21103060@users.noreply.github.com>
2022-01-26 03:04:21 +0530

    Add files via upload

---
+++ Assignment 2 answers.py
@@ -0,0 +1,54 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:/Users/Hp/Desktop/Python PEC/Assignment 2/q1.py =========
+INPUT STRING =  Python is a case sensitive language
+(Answer of a)
+35
+(Answer of b)
+egaugnal evitisnes esac a si nohtyP
+(Answer of c)
+a case sensitive
+(Answer of d)
+Python is object oriented language
+(Answer of e)
+10
+(Answer of f)
+Pythonisacasesensitivelanguage
+>>> 
+========= RESTART: C:/Users/Hp/Desktop/Python PEC/Assignment 2/q2.py =========
+Hey,Sukhmanpreet Singh Here!
+My SID is 21103060
+I am from CSE department and my CGPA is 9.9
+>>> 
+========= RESTART: C:/Users/Hp/Desktop/Python PEC/Assignment 2/q3.py =========
+Answer of A
+8
+Answer of B
+58
+Answer of C
+50
+Answer of D
+224
+40
+Answer of E
+14
+0
+>>> 
+========= RESTART: C:/Users/Hp/Desktop/Python PEC/Assignment 2/q4.py =========
+Give a number:13
+Give a number:7
+Give a number:33
+The greatest number is  33
+>>> 
+========= RESTART: C:/Users/Hp/Desktop/Python PEC/Assignment 2/q5.py =========
+Enter the string: my name is sukhmanpreet singh
+Yes
+
+>>> 
+========= RESTART: C:/Users/Hp/Desktop/Python PEC/Assignment 2/q6.py =========
+Give a number:3
+Give a number:5
+Give a number:7
+The triangle is possible
+>>> 

a2f044cda7adf65b5327abf5e37b5cf8b9353bd5
Sukhman21103060 <97335744+Sukhman21103060@users.noreply.github.com>
2022-02-14 23:27:10 +0530

    Add files via upload

---
+++ 21103060 ASSIGNMENT OUTPUTS.py
@@ -0,0 +1,72 @@
+========= RESTART: C:\Users\Hp\Desktop\Python PEC\Assignment 3\q1.py =========
+SOLUTION 1
+ENTER STRING: HELLO
+{'H': 1, 'E': 1, 'L': 2, 'O': 1}
+
+======== RESTART: C:\Users\Hp\Desktop\Python PEC\Assignment 3\q2 f.py ========
+SOLUTION 2
+ENTER THE DAY: 31
+ENTER THE MONTH OF THE YEAR: 01
+ENTER THE YEAR: 2000
+NEXT DATE IS:  1 / 2 / 2000
+
+========= RESTART: C:\Users\Hp\Desktop\Python PEC\Assignment 3\q3.py =========
+SOLUTION 3
+Enter list separate its  elements by space 3 9 10 5
+List:  3 9 10 5
+Square List: [(3, 9), (9, 81), (10, 100), (5, 25)]
+
+========= RESTART: C:\Users\Hp\Desktop\Python PEC\Assignment 3\q4.py =========
+SOLUTION 4
+Enter Your Grade Point: 9
+Your Grade is 'A' and Excellent performance
+
+========= RESTART: C:\Users\Hp\Desktop\Python PEC\Assignment 3\q5.py =========
+SOLUTION 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+========= RESTART: C:\Users\Hp\Desktop\Python PEC\Assignment 3\q6.py =========
+SOLUTION 6
+Enter SID: 21103060
+Enter Name: SUKHMAN
+Do you want to add another student entry(Y or N):Y
+Enter different SID: 21101111
+Enter Name: HELLO
+Do you want to add another student entry(Y or N):N
+<a> Student Details {21103060: 'SUKHMAN', 21101111: 'HELLO'}
+<b> Sorted Dictionary According To Student Name : {21101111: 'HELLO', 21103060: 'SUKHMAN'}
+<c> Sorted Dictionary According To Student SID : {21101111: 'HELLO', 21103060: 'SUKHMAN'}
+Search Name with help of SID: 21103060
+<d> Name Of The Student Is : SUKHMAN
+
+========= RESTART: C:\Users\Hp\Desktop\Python PEC\Assignment 3\q7.py =========
+
+
+SOLUTION 7
+ENTER THE NUMBER OF TERMS IN THE SERIES: 7
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 2.857142857142857
+
+
+========= RESTART: C:\Users\Hp\Desktop\Python PEC\Assignment 3\q8.py =========
+
+
+SOLUTION 8.
+a. {1, 3, 5, 6, 8}
+b. {3, 6, 8, 9, 13, 17}
+c. {1, 2, 4, 5}
+d. {6, 7, 8, 9, 10}
+e. {10, 7}

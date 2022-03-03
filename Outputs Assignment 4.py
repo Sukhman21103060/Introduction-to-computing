27f09a0d5d05d419d59ccd93a7b3a5b6bb6e4ea8
Sukhman21103060 <97335744+Sukhman21103060@users.noreply.github.com>
2022-03-04 01:45:31 +0530

    Add files via upload

---
+++ Outputs Assignment 4.py
@@ -0,0 +1,88 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:/Users/Hp/Desktop/Python PEC/Assignment 4/Question 1.py =====
+SOLUTION 1
+Enter number of disk in tower of Hanoi:3
+
+The Disks are numbered starting from top of the tower.
+Steps to move all disks from Source Tower to Destination Tower is given below:
+
+Move disk 1 from Source Tower to Destination
+Move disk 2 from Source Tower to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 3 from Source Tower to Destination
+Move disk 1 from Auxiliary to Source Tower
+Move disk 2 from Auxiliary to Destination
+Move disk 1 from Source Tower to Destination
+>>> 
+===== RESTART: C:/Users/Hp/Desktop/Python PEC/Assignment 4/Question 2.py =====
+SOLUTION 2
+enter the no. of rows5
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+>>> 
+===== RESTART: C:/Users/Hp/Desktop/Python PEC/Assignment 4/QUESTION 3.py =====
+SOLUTION 3
+
+Enter an Integer:3
+Enter another Integer:4
+
+The quotient when 3 is divided by 4 is 0.
+The remainder when 3 is divided by 4 is 3.
+
+SOL3.a
+Function is Not-callable
+
+SOL3.b
+All result values are NOT 'non-zero'
+
+SOL3.c
+Appended (4,5,6) in the values (0,3)
+Values greater than 4 is 5,6
+
+SOL3.d
+Above result in set form is shown below:
+{5, 6}
+
+SOL3.e
+The above set has been converted to immutable.
+
+SOL3.f
+Max value from set is 6
+Hash value of 6 is 6
+>>> 
+===== RESTART: C:/Users/Hp/Desktop/Python PEC/Assignment 4/Question 4.py =====
+SOLUTION 4
+enter name of student :sukhmanpreet singh
+Enter SID of sukhmanpreet singh: 21103060
+object created
+
+The name of the student is sukhmanpreet singh and his/her roll number is 21103060
+object deleted
+>>> 
+===== RESTART: C:/Users/Hp/Desktop/Python PEC/Assignment 4/Question 5.py =====
+SOLUTION 5
+The data is stored where:
+Mehak has a salary of 40000 rupees
+Ashok has a salary of 50000 rupees
+Viren has a salary of 60000 rupees
+All deleted, i.e. Viren
+
+>>> 
+===== RESTART: C:/Users/Hp/Desktop/Python PEC/Assignment 4/Question 6.py =====
+SOLUTION 6
+
+          WELCOME TO THE FRIENDSHIP GAME
+
+Enter Player1 name:sonik
+Enter Player2 name:amisha
+
+Enter Word by sonik:hello
+Enter Word by amisha:bye
+
+Friendship of sonik and amisha is FAKE
+>>> 
